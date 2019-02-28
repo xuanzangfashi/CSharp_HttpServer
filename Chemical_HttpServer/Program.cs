05f43f81eef11237e4e95706254f8534249f363a
xuanzangfashi <379656952@qq.com>
2019-02-28 13:04:15 +0800

    remove useless function

--- Chemical_HttpServer/Program.cs
+++ Chemical_HttpServer/Program.cs
@@ -630,39 +630,39 @@ namespace Chemical_HttpServer
             return jsonStr;
         }
 
-        private static string HandleRequest(HttpListenerRequest request, HttpListenerResponse response)
-        {
-            string data = null;
-            try
-            {
-                var byteList = new List<byte>();
-                var byteArr = new byte[2048];
-                int readLen = 0;
-                int len = 0;
-                //接收客户端传过来的数据并转成字符串类型
-                do
-                {
-                    readLen = request.InputStream.Read(byteArr, 0, byteArr.Length);
-                    len += readLen;
-                    byteList.AddRange(byteArr);
-                } while (readLen != 0);
-                data = Encoding.UTF8.GetString(byteList.ToArray(), 0, len);
+        //private static string HandleRequest(HttpListenerRequest request, HttpListenerResponse response)
+        //{
+        //    string data = null;
+        //    try
+        //    {
+        //        var byteList = new List<byte>();
+        //        var byteArr = new byte[2048];
+        //        int readLen = 0;
+        //        int len = 0;
+        //        //接收客户端传过来的数据并转成字符串类型
+        //        do
+        //        {
+        //            readLen = request.InputStream.Read(byteArr, 0, byteArr.Length);
+        //            len += readLen;
+        //            byteList.AddRange(byteArr);
+        //        } while (readLen != 0);
+        //        data = Encoding.UTF8.GetString(byteList.ToArray(), 0, len);
 
-                //获取得到数据data可以进行其他操作
-            }
-            catch (Exception ex)
-            {
-                response.StatusDescription = "404";
-                response.StatusCode = 404;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"在接收数据时发生错误:{ex.ToString()}");
-                return $"在接收数据时发生错误:{ex.ToString()}";//把服务端错误信息直接返回可能会导致信息不安全，此处仅供参考
-            }
-            response.StatusDescription = "200";//获取或设置返回给客户端的 HTTP 状态代码的文本说明。
-            response.StatusCode = 200;// 获取或设置返回给客户端的 HTTP 状态代码。
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"接收数据完成:{data.Trim()},时间：{DateTime.Now.ToString()}");
-            return $"接收数据完成";
-        }
+        //        //获取得到数据data可以进行其他操作
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        response.StatusDescription = "404";
+        //        response.StatusCode = 404;
+        //        Console.ForegroundColor = ConsoleColor.Red;
+        //        Console.WriteLine($"在接收数据时发生错误:{ex.ToString()}");
+        //        return $"在接收数据时发生错误:{ex.ToString()}";//把服务端错误信息直接返回可能会导致信息不安全，此处仅供参考
+        //    }
+        //    response.StatusDescription = "200";//获取或设置返回给客户端的 HTTP 状态代码的文本说明。
+        //    response.StatusCode = 200;// 获取或设置返回给客户端的 HTTP 状态代码。
+        //    Console.ForegroundColor = ConsoleColor.Green;
+        //    Console.WriteLine($"接收数据完成:{data.Trim()},时间：{DateTime.Now.ToString()}");
+        //    return $"接收数据完成";
+        //}
     }
 }
